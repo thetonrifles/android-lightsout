cfc9fce04f38db8a7dfd600187aa0e18f65cc42d
Andrea Petreri <andrea.petreri@gmail.com>
2016-03-18 09:15:35 +0100

    Moving view tree observer into Board class

--- app/src/main/java/com/appenjoyer/lightsout/activities/MainActivity.java
+++ app/src/main/java/com/appenjoyer/lightsout/activities/MainActivity.java
@@ -2,8 +2,6 @@ package com.appenjoyer.lightsout.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.ViewTreeObserver;
-import android.widget.GridLayout;
 import android.widget.Toast;
 
 import com.appenjoyer.lightsout.R;
@@ -12,39 +10,14 @@ import com.appenjoyer.lightsout.views.LightsOutBoard;
 
 public class MainActivity extends AppCompatActivity implements LightsOutBoard.OnItemClickListener {
 
-    private LightsOutBoard mLightsOutBoard;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLightsOutBoard = (LightsOutBoard) findViewById(R.id.grid);
-        mLightsOutBoard.setOnItemClickListener(this);
-        mLightsOutBoard.buildChildren();
 
-        mLightsOutBoard.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                final int MARGIN = 5;
-                int pWidth = mLightsOutBoard.getWidth();
-                int pHeight = mLightsOutBoard.getHeight();
-                int numOfCol = mLightsOutBoard.getColumnsCount();
-                int numOfRow = mLightsOutBoard.getRowsCount();
-                int w = pWidth / numOfCol;
-                int h = pHeight / numOfRow;
-
-                for (int yPos = 0; yPos < numOfRow; yPos++) {
-                    for (int xPos = 0; xPos < numOfCol; xPos++) {
-                        GridLayout.LayoutParams params =
-                                (GridLayout.LayoutParams) mLightsOutBoard.getChildAt(xPos, yPos).getLayoutParams();
-                        params.width = w - 2 * MARGIN;
-                        params.height = h - 2 * MARGIN;
-                        params.setMargins(MARGIN, MARGIN, MARGIN, MARGIN);
-                        mLightsOutBoard.getChildAt(xPos, yPos).setLayoutParams(params);
-                    }
-                }
-            }
-        });
+        LightsOutBoard board  = (LightsOutBoard) findViewById(R.id.grid);
+        board.setOnItemClickListener(this);
+        board.buildChildren();
     }
 
     @Override
--- app/src/main/java/com/appenjoyer/lightsout/views/LightsOutBoard.java
+++ app/src/main/java/com/appenjoyer/lightsout/views/LightsOutBoard.java
@@ -3,6 +3,7 @@ package com.appenjoyer.lightsout.views;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.GridLayout;
@@ -11,9 +12,12 @@ import com.appenjoyer.lightsout.R;
 
 public class LightsOutBoard extends FrameLayout implements View.OnClickListener {
 
+    private static final int DEFAULT_CELLS_SPACE = 2; // dp
+
     private GridLayout mGridView;
     private int mRowsCount;
     private int mColsCount;
+    private int mCellSpace;
     private OnItemClickListener mOnItemClickListener;
 
     public LightsOutBoard(Context context) {
@@ -36,13 +40,37 @@ public class LightsOutBoard extends FrameLayout implements View.OnClickListener 
             TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.LightsOutBoard);
             mRowsCount = typedArray.getInteger(R.styleable.LightsOutBoard_rows_num, 0);
             mColsCount = typedArray.getInteger(R.styleable.LightsOutBoard_cols_num, 0);
+            mCellSpace = (int) typedArray.getDimension(R.styleable.LightsOutBoard_cells_space,
+                    dpToPx(DEFAULT_CELLS_SPACE));
             typedArray.recycle();
         }
         View layout = inflate(getContext(), R.layout.view_lights_board, null);
         mGridView = (GridLayout) layout.findViewById(R.id.view_grid);
         mGridView.setRowCount(mRowsCount);
         mGridView.setColumnCount(mColsCount);
+        mGridView.post(new Runnable() {
+            @Override
+            public void run() {
+                int width = getMeasuredWidth() / getColumnsCount();
+                int height = getMeasuredHeight() / getRowsCount();
+                for (int i = 0; i < getRowsCount(); i++) {
+                    for (int j = 0; j < getColumnsCount(); j++) {
+                        GridLayout.LayoutParams params = (GridLayout.LayoutParams)
+                                getChildAt(i, j).getLayoutParams();
+                        params.width = width - 2 * mCellSpace;
+                        params.height = height - 2 * mCellSpace;
+                        params.setMargins(mCellSpace, mCellSpace, mCellSpace, mCellSpace);
+                        getChildAt(i, j).setLayoutParams(params);
+                    }
+                }
+            }
+        });
         addView(layout);
+    }
+
+    private int dpToPx(int dp) {
+        DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
+        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
     }
 
     public void buildChildren() {
